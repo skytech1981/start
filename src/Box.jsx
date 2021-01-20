fe8021a48d04bf658cfe695b9f4fcef768574aa5
sali itzhaki <sali1943@hotmail.com>
2021-01-20 12:42:24 +0200

    experiment

---
+++ src/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./box.css";
+
+
+function Box() {
+    return (
+        <div className="box">
+            <h2></h2>
+        </div>
+    )
+}
+
+export default Box
